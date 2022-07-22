ebec56ba8eec95b87866f1e306a201956a591df1
Roman Smirnov <graff049@gmail.com>
2022-07-22 10:53:30 +0300

    feature(animate_spaceship): animate spaceship asynchronously

--- animation_tools.py
+++ animation_tools.py
@@ -1,7 +1,9 @@
 import asyncio
 import curses
 import random
-import glob
+import time
+
+from curses_tools import draw_frame
 from game_constants import START_RANDINT, DIM_DURATION, NORMAL_DURATION, BRIGHT_DURATION
 
 
@@ -56,8 +58,18 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
-async def animate_spaceship():
-    frame_paths = glob.glob("frames/rocket/*.txt")
-    for path in frame_paths:
-        with open(path) as frame:
-            pass
+async def animate_spaceship(canvas, row, column):
+    rocket_frames = ['frames/rocket/rocket_frame_1.txt', 'frames/rocket/rocket_frame_2.txt']
+    with open(rocket_frames[0], 'r') as first_frame, open(rocket_frames[1], 'r') as second_frame:
+        first_file_content = first_frame.read()
+        second_file_content = second_frame.read()
+
+    animate_spaceship_tool(canvas, row, column, first_file_content)
+    draw_frame(canvas, row, column, first_file_content, negative=True)
+    animate_spaceship_tool(canvas, row, column, second_file_content)
+
+
+def animate_spaceship_tool(canvas, row, column, file_content):
+    draw_frame(canvas, row, column, file_content)
+    canvas.refresh()
+    time.sleep(1)
