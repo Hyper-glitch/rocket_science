8adadb497b7ceac59a631d176ac37a146b3b2d24
hyper-glitch <graff049@gmail.com>
2022-08-05 12:37:05 +0300

    feature(enum): add enum for key codes

--- game_constants.py
+++ game_constants.py
@@ -1,4 +1,5 @@
 """ Module with constants using in space game. """
+from enum import Enum
 
 TIC_TIMEOUT = 0.1
 DIM_DURATION = 20
@@ -9,3 +10,12 @@ STARS_AMOUNT = 500
 BORDER_THICKNESS = 2
 START_RANDINT = 0
 FRAME_RATE = 15
+
+
+class KeyCode(Enum):
+    """Class for keeping key codes digits."""
+    SPACE = 32
+    LEFT = 260
+    RIGHT = 261
+    UP = 259
+    DOWN = 258
